5864f67524baa150c8cbd040df37d03e73072efd
Helder <he7d3r@gmail.com>
2014-09-27 20:48:57 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/ConvertToSearchLinks.js
+++ src/ConvertToSearchLinks.js
@@ -4,54 +4,54 @@
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
-( function ( $, mw /* , undefined */ ) {
-'use strict';
+( function ( mw, $ ) {
+	'use strict';
 
-var convertToSearchLinks = function (){
-	var $list = $('#bodyContent').find('li'),
-		ruleSyntax = /^\s*(\S[^:]*?)\s*:\s*([\S].*?)\s*(?:\/\/.*?)?$/;
+	var convertToSearchLinks = function () {
+		var $list = $('#bodyContent').find('li'),
+			ruleSyntax = /^\s*(\S[^:]*?)\s*:\s*([\S].*?)\s*(?:\/\/.*?)?$/;
 
-	$list.each(function(){
-		var	$item = $(this),
-			searchURL = mw.util.getUrl( 'Special:Search', { fulltext: 1, search: '"' } ),
-			match = ruleSyntax.exec( $item.text() );
-		// Current syntax: * old word : new word //Some optional comment
-		if( match && match[1] && match[2]) {
-			/*jshint unused:false */
-			$item.html( function( index, oldhtml ){
-				var link = mw.html.element(
-						'a', {
-							href: searchURL + match[1] + '"' ,
-							title: 'Pesquisar ocorrências deste termo na wiki'
-						}, match[1]
-					),
-					newRule = link + ': ' + match[2] + ' ' + match[3];
-				return oldhtml.replace( ruleSyntax,	newRule);
-			});
-			/*jshint unused:true */
-		}
-	});
-};
+		$list.each(function () {
+			var	$item = $(this),
+				searchURL = mw.util.getUrl( 'Special:Search', { fulltext: 1, search: '"' } ),
+				match = ruleSyntax.exec( $item.text() );
+			// Current syntax: * old word : new word //Some optional comment
+			if ( match && match[1] && match[2]) {
+				/*jshint unused:false */
+				$item.html( function ( index, oldhtml ) {
+					var link = mw.html.element(
+							'a', {
+								href: searchURL + match[1] + '"',
+								title: 'Pesquisar ocorrências deste termo na wiki'
+							}, match[1]
+						),
+						newRule = link + ': ' + match[2] + ' ' + match[3];
+					return oldhtml.replace( ruleSyntax,	newRule);
+				});
+				/*jshint unused:true */
+			}
+		});
+	};
 
-if ( $.inArray( mw.config.get('wgPageName'),
-	[
-		'Wikipédia:Dicionário',
-		'Wikipédia:Dicionário/pt-AO',
-		'Wikipédia:Dicionário/pt-BR',
-		'Wikipédia:Dicionário/pt-PT',
-		'Wikisource:Modernização/Dicionário',
-		'Wikisource:Modernização/Dicionário/pt-BR',
-		'Wikisource:Modernização/Dicionário/pt-PT'
-	] ) !== -1 ) {
-	mw.loader.load(
-		'//meta.wikimedia.org/w/index.php?title=User:He7d3r/Tools/LanguageConverterDistances.js&action=raw&ctype=text/javascript'
-	);
-	$(mw.util.addPortletLink( 'p-cactions', '#', 'Convert to search links',
-		'ca-convert-to-search', 'Replace the left hand side of each convertion rule by a link to the search page'
-	)).click( function( e ) {
-		e.preventDefault();
-		convertToSearchLinks();
-	});
-}
+	if ( $.inArray( mw.config.get('wgPageName'),
+		[
+			'Wikipédia:Dicionário',
+			'Wikipédia:Dicionário/pt-AO',
+			'Wikipédia:Dicionário/pt-BR',
+			'Wikipédia:Dicionário/pt-PT',
+			'Wikisource:Modernização/Dicionário',
+			'Wikisource:Modernização/Dicionário/pt-BR',
+			'Wikisource:Modernização/Dicionário/pt-PT'
+		] ) !== -1 ) {
+		mw.loader.load(
+			'//meta.wikimedia.org/w/index.php?title=User:He7d3r/Tools/LanguageConverterDistances.js&action=raw&ctype=text/javascript'
+		);
+		$(mw.util.addPortletLink( 'p-cactions', '#', 'Convert to search links',
+			'ca-convert-to-search', 'Replace the left hand side of each convertion rule by a link to the search page'
+		)).click( function ( e ) {
+			e.preventDefault();
+			convertToSearchLinks();
+		});
+	}
 
-}( jQuery, mediaWiki ) );
+}( mediaWiki, jQuery ) );
